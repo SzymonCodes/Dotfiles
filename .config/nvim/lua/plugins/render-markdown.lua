60ea358305733593f58298888c9503bbb7e94d9b
Szymon Zubek <szymon@MacBookPro.mynet>
2025-04-11 19:51:49 +0100

    Changing LSP + CMP setup
    Updated the LSP + CMP setup using Typecraft's videos. Also went back and
    changed the Aerospace shortcuts so that I use Alt+Q/W/E/R/T to move
    between each screen.

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -1,10 +1,8 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-	---@module 'render-markdown'
-	---@type render.md.UserConfig
-	opts = {},
-	config = function()
-		require("render-markdown").setup()
-	end,
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+  opts = {},
+  config = function()
+    require("render-markdown").setup()
+  end,
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "python", "markdown", "markdown_inline", "sql" },
+      auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
