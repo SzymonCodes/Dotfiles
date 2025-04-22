ae5643d18d59ee5241153781e884696338a335a2
Simon <zubekszymon@gmail.com>
2025-04-23 00:59:51 +0100

    Adding a Nix config

---
+++ nix/flake.nix
@@ -0,0 +1,157 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+    let
+      configuration = { pkgs, config, ... }: {
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+
+        nixpkgs.config.allowUnfree = true;
+
+        environment.systemPackages =
+          [ 
+            # IDE
+            pkgs.neovim
+            pkgs.tmux
+            pkgs.ghostty
+            pkgs.duckdb
+            pkgs.tidy-viewer
+            pkgs.quarto
+
+            # CLIs 
+            pkgs.starship
+            pkgs.fzf
+            pkgs.neofetch
+            pkgs.stow
+            pkgs.zoxide
+            pkgs.zsh-autosuggestions
+            pkgs.zsh-syntax-highlighting
+            pkgs.bat
+            pkgs.ripgrep
+            pkgs.cmatrix
+
+            # GitHub
+            pkgs.git
+            pkgs.git-credential-manager
+
+            # Language servers for Neovim
+            pkgs.lua-language-server
+            pkgs.pyright
+
+            # Apps
+            pkgs.aerospace
+            pkgs.appcleaner
+            pkgs.google-chrome
+            pkgs.obsidian
+            pkgs.spotify
+            pkgs.raycast
+            pkgs._1password-gui
+            pkgs._1password-cli
+
+            # System
+            pkgs.mkalias
+          ];
+
+        homebrew = [
+          enable = true;
+          core = [
+            "borders";
+          ];
+          taps = [
+            "FelixKratz/formulae":
+          ];
+          casks = [
+            "affinity-designer";
+            "affinity-photo";
+            "logi-options+";
+          ];
+          onActivation.cleanup = "zap";
+          onActivation.autoUpdate = true;
+          onActivation.upgrade = true;
+        ];
+
+        fonts.packages = [
+          (pkgs.nerdfonts.override { fonts = ["JetBrainsMono"]; });
+        ];
+
+        system.activationScripts.applications.text = let
+          env = pkgs.buildEnv {
+            name = "system-applications";
+            paths = config.environment.systemPackages;
+            pathsToLink = "/Applications";
+          };
+        in
+          pkgs.lib.mkForce ''
+            # Set up applications.
+            echo "setting up /Applications..." >&2
+            rm -rf /Applications/Nix\ Apps
+            mkdir -p /Applications/Nix\ Apps
+            find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+            while read -r src; do
+              app_name=$(basename "$src")
+              echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+            done
+          '';
+
+        system.defaults = {
+          dock.autohide = true;
+          dock.wvous-bl-corner = 1;
+          dock.persistent-apps = [
+              "${pkgs.ghostty}/Applications/Ghostty.app";
+              "${pkgs.obsidian}/Applications/Obsidian.app";
+              "${pkgs.google-chrome}/Applications/Google Chrome.app";
+              "${pkgs.spotify}/Applications/Spotify.app";
+              "/Applications/Music.app";
+              "${pkgs.appcleaner}/Applications/AppCleaner.app";
+          ];
+          finder.FXPrefferedViewStyle = "Nlsv";
+          loginwindow.GuestEnabled = false;
+          NSGlobalDomain.AppleICUForce24HourTime = true;
+          NSGlobalDomain.AppleInterfaceStyle = "Dark";
+          NSGlobalDomain.KeyRepeat = 2;
+        };
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Enable alternative shell support in nix-darwin.
+        # programs.fish.enable = true;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 6;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
+    in
+      {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
+        modules = [ 
+          configuration 
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+
+              user = "szymon";
+            }
+          }
+        ];
+      };
+    };
+};
